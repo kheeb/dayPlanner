ac9e7cd41705900e9fe319c411191b520e80d442
Kara Heeb <karaheeb@gmail.com>
2021-12-17 00:25:16 -0500

    set time and color coding, changed color schemes, updated fonts, added additional timeblock rows, and updated README

--- README.md
+++ README.md
@@ -1,23 +1,5 @@
 # dayPlanner
-
- ## Acceptance Criteria
-
-```md
-GIVEN I am using a daily planner to create a schedule
-WHEN I open the planner
-THEN the current day is displayed at the top of the calendar
-WHEN I scroll down
-THEN I am presented with timeblocks for standard business hours
-WHEN I view the timeblocks for that day
-THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-WHEN I click into a timeblock
-THEN I can enter an event
-WHEN I click the save button for that timeblock
-THEN the text for that event is saved in local storage
-WHEN I refresh the page
-THEN the saved events persist
-```
-
-The following animation demonstrates the application functionality:
-
-![A user clicks on slots on the color-coded calendar and edits the events.](./Assets/05-third-party-apis-homework-demo.gif)
+This is a bit of refactored code that allows a user to create a schedule.
+The planner displays the current day above hourly timeblocks.
+Users can enter text beside the desired timeblock and save their plans for later.
+The scheduler automatically color codes past, present, and future events.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
-console.log("Hello World")
-
+//  store value from text area to local storage
 $(".saveBtn").on("click" , function(){
     var text = $(this).siblings("textarea").val()
     console.log(text)
@@ -7,6 +6,40 @@ $(".saveBtn").on("click" , function(){
     localStorage.setItem(time, text)
 })
 
+// get item from storage to page after refreshing
+$("#8").val(localStorage.getItem(8))
 $("#9").val(localStorage.getItem(9))
 $("#10").val(localStorage.getItem(10))
 $("#11").val(localStorage.getItem(11))
+$("#12").val(localStorage.getItem(12))
+$("#13").val(localStorage.getItem(13))
+$("#14").val(localStorage.getItem(14))
+$("#15").val(localStorage.getItem(15))
+$("#16").val(localStorage.getItem(16))
+$("#17").val(localStorage.getItem(17))
+
+// set currentDay
+var today = moment().format("MMM Do, YYYY");
+$("#currentDay").text(today);
+
+// set the time by hour
+var now = moment().hour();
+console.log(now);
+
+// create a variable for HTML textarea tag
+var textArea = $("textarea");
+console.log(textArea);
+
+// sync timeblocks by hour
+for (var i=0; i < textArea.length; i++) {
+    console.log(textArea[i]);
+    console.log($(textArea[i]).attr("id"));
+    var timeBlockHour = $(textArea[i]).attr("id");
+    if (timeBlockHour === now) {
+        $(textArea[i]).addClass("present");
+    } else if (timeBlockHour < now) {
+        $(textArea[i]).addClass("past");
+    } else {
+        $(textArea[i]).addClass("future")
+    }
+}
